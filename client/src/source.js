3333e65ff97eafa51727cbf71c8e6d9703f025fc
yashas <yashu.gsb@gmail.com>
2019-06-11 02:32:51 +0530

    commit messageas

--- client/src/source.js
+++ client/src/source.js
@@ -7,7 +7,7 @@ const initialState={};
 const store =createStore(rootReducer,
     initialState,
     compose(applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() || compose
     ))
 
 export default store;
